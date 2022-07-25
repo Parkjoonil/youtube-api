cc2167781f4931988c101f4d57542335e0621101
Parkjoonil <106866130+Parkjoonil@users.noreply.github.com>
2022-07-26 02:24:04 +0900

    delete api_key

--- src/app/service/search/search.service.ts
+++ src/app/service/search/search.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class SearchService {
   private API_URL = 'https://www.googleapis.com/youtube/v3/search';
-  private API_KEY = 'AIzaSyBJkA_NBssyQV_Ps31PKm6tw5ZExE7cf34';
+  private API_KEY = '';
   constructor(private http: HttpClient) {}
   getVideos(query: string): Observable <any> {
     const url = `${this.API_URL}?q=${query}&key=${this.API_KEY}&part=snippet&type=video&maxResults=10`;
